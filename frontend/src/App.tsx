d1cfc7914b651b4db780a3809288f7f2ed859ccd
JamesHenso <sieunhangao09123@gmail.com>
2026-04-14 17:00:44 +0700

    refactor(frontend): update application layout and global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import { Routes, Route, useLocation } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import MainPage from "./pages/MainPage";
 
 export default function App() {
-  const location = useLocation();
-  const hideNavbarPaths = ["/MainPage"];
-  return (
-    <>
-     {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/MainPage" element={<MainPage />} />
-      </Routes>
-    </>
-  )
+    const location = useLocation();
+    const hideNavbarPaths = ["/MainPage"];
+    return (
+        <>
+            {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/MainPage" element={<MainPage />} />
+            </Routes>
+        </>
+    );
 }
