7c9e2d4d2c81055937cddb758b7ede18b985e5b4
JamesHenso <sieunhangao09123@gmail.com>
2026-04-27 20:39:25 +0700

    update: call API for conversation

---
+++ frontend/src/api/chatAPI.ts
@@ -0,0 +1,104 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8000/api/v1/chat/conversations";
+
+export interface Message {
+	id: string;
+	role: "user" | "bot";
+	content: string;
+	image_url?: string | null;
+	created_at: string | Date;
+}
+
+export interface Conversation {
+	id: string;
+	title: string;
+	created_at: string | Date;
+	updated_at: string | Date;
+}
+
+export interface ConversationDetail extends Conversation {
+	messages: Message[];
+}
+
+export interface MessageCreatePayload {
+	role: "user" | "assistant" | "system";
+	content: string;
+	image_url?: string | null;
+}
+
+export const getConversations = async (
+	token: string,
+): Promise<Conversation[]> => {
+	const reponse = await axios.get<Conversation[]>(`${API_BASE_URL}/`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+	});
+	return reponse.data;
+};
+
+export const createConversation = async (
+	token: string,
+	title: string,
+): Promise<Conversation> => {
+	const reponse = await axios.post<Conversation>(
+		`${API_BASE_URL}/`,
+		{ title },
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		},
+	);
+	return reponse.data;
+};
+
+export const getConversationDetail = async (
+	token: string,
+	convID: string,
+): Promise<ConversationDetail> => {
+	const response = await axios.get<ConversationDetail>(
+		`${API_BASE_URL}/${convID}/`,
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		},
+	);
+	return response.data;
+};
+
+export const sendMessage = async (
+	token: string,
+	convId: string,
+	payload: MessageCreatePayload,
+): Promise<Message> => {
+	const response = await axios.post<Message>(
+		`${API_BASE_URL}/${convId}/messages/`,
+		payload,
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		},
+	);
+	return response.data;
+};
+
+export const deleteMessage = async (
+	token: string,
+	convID: string,
+): Promise<boolean> => {
+	const response = await axios.delete(`${API_BASE_URL}/${convID}/`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+	});
+	return response.status >= 200 && response.status < 300;
+};
